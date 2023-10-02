9feb50c05bc123c0784d8ec74a8a4018b21a3363
Adeondocarmo <146399192+Adeondocarmo@users.noreply.github.com>
2023-10-01 23:09:15 -0300

    opção de sair
    incluso no sistema a opção de sair

--- atividadeG2.cpp
+++ atividadeG2.cpp
@@ -120,7 +120,8 @@ int main()
  	   printf("Escolha a opção desejada do menu\n\n");
 	   printf("\t1-Registrar nomes\n");
  	   printf("\t2-Consultar nomes\n");
-	   printf("\t3-deletar nomes\n\n");
+	   printf("\t3-Deletar nomes\n\n");
+	   printf("\t4-Sair do sistema\n\n");
 	   printf("opção:");//fim do menu
 	
 	   scanf("%d",&opcao);
@@ -140,7 +141,12 @@ int main()
 		 case 3:
 		 deletar();
 		 break;
-		 ;
+		
+		 case 4:
+		 printf("obrigado por utilizar o sistema!\n");
+		 return 0;
+		 break;
+		 	
 		 default:
 		 printf("Essa opção não está disponivel\n");
 		 system("pause");
